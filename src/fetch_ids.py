eaca5baf33dfd1cb422934238443908d075cd9b8
Shin Ando <172533732+S-Ando-Biophysics@users.noreply.github.com>
2026-03-19 20:09:51 +0900

    Update fetch_ids.py

--- src/fetch_ids.py
+++ src/fetch_ids.py
@@ -146,7 +146,7 @@ def main() -> None:
     pdb_ids = search_pdb_ids()
     
     # Only When testing (limitation of number) --> Comment out
-    pdb_ids = pdb_ids[:100]
+    pdb_ids = pdb_ids[:5]
     
     print(f"Found {len(pdb_ids)} entries for processing.")
     
